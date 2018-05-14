a8906e533735b072909be5fd06ea56b69aae35ab
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-05-14 21:23:50 +1000

    added working deauney mesh for projet layout card

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -9,6 +9,12 @@ import { poissonDiscSampler, sample } from '../lib/utils';
 import AboutPanel from './AboutPanel';
 import AboutAnimation from './AboutAnimation';
 
+const ProjectCardWrapper = styled.div`
+  canvas {
+    ${'' /* transform: translateY(-15px) translateX(-15px); */}
+  }
+  ${'' /* overflow: hidden; */}
+`
 const CardTitle = styled.div`
   z-index: 10;
   position: absolute;
@@ -27,11 +33,11 @@ class ProjectsCard extends React.Component {
       super(props);
   }
   startAnimation() {
-    var height = 280;
-    var width = 350;
+    var height = 250;
+    var width = 309;
     var pi = 2 * Math.PI;
     var gravity = 0.05;
-    var sample = poissonDiscSampler(width, height, 30);
+    var sample = poissonDiscSampler(width+200, height+200, 20);
     var s;
     var canvas = d3.select('.project-card-container').append('canvas')
       .attr('width', width)
@@ -42,25 +48,27 @@ class ProjectsCard extends React.Component {
     var voronoi = d3.voronoi()
       .x(function (d) { return d.x })
       .y(function (d) { return d.y })
-      // .extent([[0, 0], [height, width]])
+      // .size(width, height)
+      // .extent([[-1, -1], [width +100, height +100 ]])
     var links = voronoi.links(nodes)
+
     var force = d3.forceSimulation()
       .nodes(nodes.slice())
-      .force('charge', d3.forceManyBody().strength(function (d, i) { return i ? -30 : -300}))
+      .force('charge', d3.forceManyBody().strength(function (d, i) { return i ? -30 : -180}))
+      .alphaTarget(0.1)
       .on('tick', ticked)
     d3.select('.project-card-container')
       .on('touchmove mousemove', moved)
-    var root = nodes.shift()
-    root.fixed = false;
     function moved() {
       var p1 = d3.mouse(this)
-      root.x = p1[0]
-      root.y = p1[1]
-      nodes[0] = p1
-      force.alphaTarget(0.3).restart()
+      nodes[0].x = p1[0]
+      nodes[0].y = p1[1]
+      force.restart()
     };
     function ticked () {
+      
       force.restart()
+      
       for (var i = 0, n = nodes.length; i < n; ++i) {
         var node = nodes[i];
         node.y += (node.cy - node.y) * gravity;
@@ -102,6 +110,7 @@ class ProjectsCard extends React.Component {
   render() {
     
     return (
+      <ProjectCardWrapper>
       <Paper
         style={{ height: 250, width: 309, display: 'inline-block'}}>
           <div className='project-card-container' >   
@@ -110,6 +119,7 @@ class ProjectsCard extends React.Component {
           </CardTitle>
         </div>
       </Paper>
+      </ProjectCardWrapper>
     )
 }
 }

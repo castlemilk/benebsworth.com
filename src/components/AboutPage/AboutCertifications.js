128a6980a955b995945532532df2e0fad5e910b0
Ben Ebsworth <ben.ebsworth@digio.com.au>
2018-05-28 06:48:27 +1000

    added certification skeleton

---
+++ src/components/AboutPage/AboutCertifications.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components';
+const Wrapper = styled.div`
+`;
+const certs = {
+    
+}
+
+const AboutCertifications = (props) => (
+    <Wrapper>
+
+    </Wrapper>
+)

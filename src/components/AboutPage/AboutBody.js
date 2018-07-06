feedf8b228f7e6d8e69207c02786bf61fcf26422
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-07-06 21:29:37 +1000

    fixed typos

--- src/components/AboutPage/AboutBody.js
+++ src/components/AboutPage/AboutBody.js
@@ -46,10 +46,10 @@ const AboutBlurb = styled.div`
 
 const bio1 =`
 Oh so you'd like to know a little about me? Well I'm flattered. Here we go; Born to a Moroccan Diesel Mechanic and Australian Artist.
-             We lived in Port Hedland, Western Australia for 8 years before moving to a very small down called Denmark in Western Australia.
+             We lived in Port Hedland, Western Australia for 8 years before moving to a very small town called Denmark in Western Australia.
              These small towns, with the typical small town mentalities proved challenging, but somewhat enriching. Finally we settled in Melbourne where I've been since
-             the start of my highschool years.
-             I only just made it into what could be described as a more driven mode of operation. I am deeply passionate about learning and developing my understanding of the world, perhaps deriving personal existential meaning through this endeavour.
+             the start of my high school years.
+             I only just made it into what could be described as a more driven mode of operation. I'm deeply passionate about learning and developing my understanding of the world, perhaps deriving personal existential meaning through this endeavour.
              Strongly rooted in a belief of nurture over nature, and that effort will often trump gifted intelligence. Setting out to prove myself right by working diligently to make my dreams come to fruition.
              I bloomed late in a sense, unfortunately I didn't have any early tutillage to guide me into intellectual pursuits. So I am catching up, albeit quickly, if I can say so myself.
 
